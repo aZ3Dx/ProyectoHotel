7331bde3c7bdccd05542f3fda36632e3f1d71c35
Jean Herrera <jeankevinherrera@gmail.com>
2023-06-09 15:45:42 -0500

    first commit

---
+++ ProyectoHotel/Logica/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoHotel.Logica
+{
+    public class Conexion
+    {
+        //public static string CN = "Data Source=DESKTOP-0S4FHNI;Initial Catalog=DB_HOTEL;Integrated Security=True";
+        public static string CN = "Data Source=localhost;Initial Catalog=DB_HOTEL;User Id=sa;Password=!P4nqueque";
+    }
+}
